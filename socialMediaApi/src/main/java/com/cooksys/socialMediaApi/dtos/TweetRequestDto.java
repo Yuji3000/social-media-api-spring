f12677b252aa4eec63e901d8146412a74d01cf4c
thomas <26800120+Blazin64@users.noreply.github.com>
2024-08-26 21:00:11 -0500

    TweetRequestDto should only have content and credentials fields

--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/dtos/TweetRequestDto.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/dtos/TweetRequestDto.java
@@ -3,20 +3,12 @@ package com.cooksys.socialMediaApi.dtos;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
-
 @NoArgsConstructor
 @Data
 public class TweetRequestDto {
 
-    private UserResponseDto author;
-
-    private Timestamp posted;
-
     private String content;
 
-    private TweetResponseDto inReplyTo;
-
-    private TweetResponseDto repostOf;
+    private CredentialsDto credentials;
 
 }
