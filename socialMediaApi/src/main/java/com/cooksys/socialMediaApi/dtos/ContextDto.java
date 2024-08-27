26a991f59c57cdbfa2248fdd4db534112fb62894
thomas <26800120+Blazin64@users.noreply.github.com>
2024-08-26 19:53:38 -0500

    Added ContextDto

---
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/dtos/ContextDto.java
@@ -0,0 +1,17 @@
+package com.cooksys.socialMediaApi.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+public class ContextDto {
+
+    private TweetResponseDto target;
+
+    private List<TweetResponseDto> before;
+
+    private List<TweetResponseDto> after;
+}
