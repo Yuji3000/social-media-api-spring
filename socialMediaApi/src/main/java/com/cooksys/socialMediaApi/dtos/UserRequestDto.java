6404bd269eeea07717244b15ff9d7ad467724945
thomas <26800120+Blazin64@users.noreply.github.com>
2024-08-26 20:33:13 -0500

    UserRequestDto correction

--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/dtos/UserRequestDto.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/dtos/UserRequestDto.java
@@ -3,8 +3,6 @@ package com.cooksys.socialMediaApi.dtos;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
-
 @NoArgsConstructor
 @Data
 public class UserRequestDto {
