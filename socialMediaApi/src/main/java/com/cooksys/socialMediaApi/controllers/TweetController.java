6329364e0e4364c1576a7ea2dbe9890623d9ee2f
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-08-26 22:09:47 -0400

    Add TweetController.java

---
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/controllers/TweetController.java
@@ -0,0 +1,14 @@
+package com.cooksys.socialMediaApi.controllers;
+
+import com.cooksys.socialMediaApi.services.TweetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tweets")
+public class TweetController {
+
+    private final TweetService tweetService;
+}
