cdaf427c5c312328f9cf55f2f7ba67c5a48bc967
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-08-26 20:46:23 -0400

    Add UserRepository.java

---
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cooksys.socialMediaApi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
