bb9f9f8ca1d89b6b21564150fe8386f8d94a3e36
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-08-26 20:32:37 -0600

    Add embeddable profile and updated hashtag and tweet repository

--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/repositories/HashtagRepository.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/repositories/HashtagRepository.java
@@ -1,9 +1,11 @@
 package com.cooksys.socialMediaApi.repositories;
 
-import com.cooksys.socialMediaApi.entities.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cooksys.socialMediaApi.entities.Hashtag;
+
 @Repository
-public interface HashtagRepository extends JpaRepository<HashTag, Long> {
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+	
 }
--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/repositories/TweetRepository.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/repositories/TweetRepository.java
@@ -3,6 +3,8 @@ package com.cooksys.socialMediaApi.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cooksys.socialMediaApi.entities.Tweet;
+
 // TODO: Add Tweet entity import
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
