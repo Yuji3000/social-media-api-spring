80804744a174f6dc06f100734a8cc6fcc83bda64
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-08-29 17:07:48 -0600

    Add tweet/id/mentions endpoint

--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/TweetService.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/TweetService.java
@@ -3,8 +3,11 @@ package com.cooksys.socialMediaApi.services;
 import java.util.List;
 
 import com.cooksys.socialMediaApi.dtos.TweetResponseDto;
+import com.cooksys.socialMediaApi.dtos.UserResponseDto;
 
 public interface TweetService {
 
 	List<TweetResponseDto> getAllTweets();
+
+	List<UserResponseDto> getTweetMentions(Long id);
 }
--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/impl/TweetServiceImpl.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/impl/TweetServiceImpl.java
@@ -1,11 +1,18 @@
 package com.cooksys.socialMediaApi.services.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.cooksys.socialMediaApi.dtos.TweetResponseDto;
+import com.cooksys.socialMediaApi.dtos.UserResponseDto;
+import com.cooksys.socialMediaApi.entities.Tweet;
+import com.cooksys.socialMediaApi.entities.User;
+import com.cooksys.socialMediaApi.exceptions.NotFoundException;
 import com.cooksys.socialMediaApi.mappers.TweetMapper;
+import com.cooksys.socialMediaApi.mappers.UserMapper;
 import com.cooksys.socialMediaApi.repositories.TweetRepository;
 import com.cooksys.socialMediaApi.services.TweetService;
 
@@ -17,9 +24,25 @@ public class TweetServiceImpl implements TweetService {
 
     private final TweetRepository tweetRepository;
     private final TweetMapper tweetMapper;
+    private final UserMapper userMapper;
     
 	@Override
 	public List<TweetResponseDto> getAllTweets() {
 		return tweetMapper.entitiesToDtos(tweetRepository.findByDeletedFalseOrderByPostedDesc());
+	}
+
+	@Override
+	public List<UserResponseDto> getTweetMentions(Long id) {
+		Optional<Tweet> optionalTweet = tweetRepository.findByIdAndDeletedFalse(id);
+		if (optionalTweet.isEmpty()) {
+			throw new NotFoundException("Tweet not found with ID:" + id);
+		}
+		Tweet tweet = optionalTweet.get();
+		List<User> mentionedUsers = tweet.getMentionedUsers()
+				.stream()
+				.filter(user -> !user.isDeleted())
+	            .collect(Collectors.toList());
+		
+		return userMapper.entitiesToDtos(mentionedUsers);
 	}
 }
