08bbad433e6942e0d2825813cabf7f7606df03e0
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-08-29 18:16:20 -0400

    Add getTagOrCreateIfNew for HashtagService

--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/HashtagService.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/HashtagService.java
@@ -1,4 +1,8 @@
 package com.cooksys.socialMediaApi.services;
 
+import com.cooksys.socialMediaApi.entities.Hashtag;
+
 public interface HashtagService {
+
+    Hashtag getTagOrCreateIfNew(String label);
 }
--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/impl/HashtagServiceImpl.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/services/impl/HashtagServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cooksys.socialMediaApi.services.impl;
 
+import com.cooksys.socialMediaApi.entities.Hashtag;
 import com.cooksys.socialMediaApi.repositories.HashtagRepository;
 import com.cooksys.socialMediaApi.services.HashtagService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,17 @@ import org.springframework.stereotype.Service;
 public class HashtagServiceImpl implements HashtagService {
 
     private final HashtagRepository hashtagRepository;
+
+    @Override
+    public Hashtag getTagOrCreateIfNew(String label) {
+        Optional<Hashtag> optionalHashtag = hashtagRepository.findByLabel(label);
+
+        if (optionalHashtag.isEmpty()) {
+            var newHashTag = new Hashtag();
+            newHashTag.setLabel(label);
+            return hashtagRepository.saveAndFlush(newHashTag);
+        }
+
+        return optionalHashtag.get();
+    }
 }
