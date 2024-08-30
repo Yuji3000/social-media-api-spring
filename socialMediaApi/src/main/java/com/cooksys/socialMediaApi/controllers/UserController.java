432d50094ad44e16c74f62508141d6607e66eb4c
thomas <26800120+Blazin64@users.noreply.github.com>
2024-08-30 17:44:31 -0500

    Fix import

--- socialMediaApi/src/main/java/com/cooksys/socialMediaApi/controllers/UserController.java
+++ socialMediaApi/src/main/java/com/cooksys/socialMediaApi/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.cooksys.socialMediaApi.controllers;
 import java.util.List;
 
 import com.cooksys.socialMediaApi.entities.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.cooksys.socialMediaApi.dtos.CredentialsDto;
 import com.cooksys.socialMediaApi.dtos.TweetResponseDto;
@@ -68,7 +69,7 @@ public class UserController {
      return userService.getFollowingUsers(username);
    }
 
- @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
 	@PostMapping("/@{username}/follow")
 	public void followUser(@PathVariable String username, @RequestBody CredentialsDto credentialsDto) {
 		User follower = userService.authenticateUser(credentialsDto);
